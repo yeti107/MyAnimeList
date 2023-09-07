768f8671f16ea82b9f0797858c09f0a7f88da269
Munaan <r-munaan@outlook.com>
2023-09-07 17:22:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+import'./components/style.css' ;
+import { AnimeList } from './components/AnimeList';
+import { AnimeInfo } from './components/AnimeInfo';
+import { AddToList } from './components/AddToList';
+import { RemoveFromList } from './components/RemoveFromList';
+
+function App() {
+
+  const [search,setSearch] = useState('vinland saga')
+  const [animeData, setanimeData]=useState()
+  const [animeInfo, setAnimeInfo] =useState()
+  const [myAnimeList, setMyAnimeList] = useState ([])
+
+  const addTo=(anime)=> {
+    const index=myAnimeList.findIndex((myanime)=> {
+           return myanime.mal_id === anime.mal_id
+    })
+   if(index < 0) {
+    const newArray=[...myAnimeList,anime]
+     setMyAnimeList(newArray);
+   }
+
+  }
+  const removeFrom=(anime)=>{
+    const newArray=myAnimeList.filter((myanime)=>{
+      return myanime.mal_id !== anime.mal_id  //checks if anime is already in list so it can be removed
+    })
+
+    setMyAnimeList(newArray)
+  }
+
+  const getData=async()=>{
+    const res=await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=20`)
+    const resData= await res.json();
+    setanimeData(resData.data)
+   }
+   useEffect(()=>{
+    getData()
+   }, [search])
+
+
+  return (
+    <>
+    <div className="header">
+      <h1>MyAnimeList</h1>
+      <div className="search-box">
+        <input type="search"placeholder="Search your anime" 
+        onChange={(e)=>setSearch(e.target.value)}/>
+      </div>
+      </div>
+    
+    <div className="container">
+         <div className='animeInfo'>
+          {animeInfo && <AnimeInfo animeInfo={animeInfo}/>}
+
+         </div>
+         <div className='anime-row'>
+          <h2 className='text-heading'>Anime</h2>
+          <div className='row'>
+               <AnimeList 
+               animelist={animeData}
+               setAnimeInfo={setAnimeInfo}
+               animeComponent={AddToList}     
+               handleList={(anime)=>addTo(anime)}   
+               />
+          </div>
+          <h2 className='text-heading'>My List</h2>
+          <div className='row'>
+               <AnimeList 
+               animelist={myAnimeList}
+               setAnimeInfo={setAnimeInfo}
+               animeComponent={RemoveFromList}     
+               handleList={(anime)=>removeFrom(anime)}   
+               />
+          </div>
+         </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
